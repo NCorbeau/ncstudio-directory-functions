9cb8f314ad2ae5b5742c6094562fc1ff5c9afcb2
NCorbeau <glownia@gmail.com>
2025-05-08 21:41:11 +0200

    Add max-age header

--- src/index.js
+++ src/index.js
@@ -23,7 +23,8 @@ export default {
     const corsHeaders = {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+       'Access-Control-Max-Age': '86400'
     };
 
     // Handle OPTIONS requests (CORS preflight)
